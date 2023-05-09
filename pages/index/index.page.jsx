84c342f40ed7985c8ac3133e524b55aa39cc0503
Ronaldo Suarez Surface <ronaldo3055@gmail.com>
2023-05-09 01:36:26 -0400

    started services page

--- pages/index/index.page.jsx
+++ pages/index/index.page.jsx
@@ -11,8 +11,16 @@ function Page() {
       <Nav />
       <Landing />
       <WhyUs />
-      <div className="w-full">
+      <div className="w-full mt-16">
+        <h2 className="mb-4 text-4xl text-center font-header-medium">
+          Our Services
+        </h2>
+        <h3 className="mb-8 text-3xl text-black max-w-md mx-auto text-center font-header">
+          No job is too big or too small for us. We got you covered.
+        </h3>
+        <div className="grid grid-cols-2">
 
+        </div>
       </div>
     </>
   );
