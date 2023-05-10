484aced015ec17b972da2418c2ace20f6aef26ee
Ronaldo Suarez <ronaldo3055@gmail.com>
2023-05-10 13:04:35 -0400

    fix svg import

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -6,7 +6,6 @@ import ReactDOMServer from 'react-dom/server'
 import React from 'react'
 import { PageShell } from './PageShell'
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
-import logoUrl from './logo.svg'
 
 async function render(pageContext) {
   const { Page, pageProps } = pageContext
@@ -27,7 +26,6 @@ async function render(pageContext) {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <!-- <link rel="icon" href="${logoUrl}" /> -->
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
